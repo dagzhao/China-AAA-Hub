f9efa7eff531fac77cacfde2b9b897edd288f1da
Jingtao Zhao <dagzhao@gmail.com>
2026-02-06 09:48:56 +0800

    add speed

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -1,9 +1,18 @@
 import DefaultTheme from 'vitepress/theme'
-import { injectSpeedInsights } from '@vercel/speed-insights'
 import './custom.css'
 
+// 仅在浏览器环境下执行
 if (typeof window !== 'undefined') {
-  injectSpeedInsights();
+  // 导入并执行 Analytics
+  import('@vercel/analytics').then(({ inject }) => inject());
+
+  // 导入并执行 Speed Insights
+  import('@vercel/speed-insights').then(({ injectSpeedInsights }) => injectSpeedInsights());
 }
 
-export default DefaultTheme
+export default {
+  ...DefaultTheme, // 使用扩展语法继承默认主题
+  enhanceApp({ app, router, siteData }) {
+    // 以后如果你有自定义组件或全局插件，可以在这里添加
+  }
+}
